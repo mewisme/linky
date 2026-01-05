e2b63c6191da62dc4e95855ff620f15b73a3dea4
Mew <mauminh.nguyen@gmail.com>
2026-01-05 22:38:13 +0700

    feat: hide specific navbar element in user profile page for improved UI

--- apps/web/app/(auth)/user-profile/[[...user-profile]]/page.tsx
+++ apps/web/app/(auth)/user-profile/[[...user-profile]]/page.tsx
@@ -7,6 +7,12 @@ import { dark } from '@clerk/themes'
 export default function UserProfilePage() {
   return (
     <main className="flex min-h-screen items-center justify-center p-4">
+      {/* eslint-disable-next-line react/no-unknown-property */}
+      <style jsx global>{`
+        .cl-navbar > div:nth-child(3) {
+          display: none !important;
+        }
+      `}</style>
       <SignedIn>
         <UserProfile appearance={{
           baseTheme: dark

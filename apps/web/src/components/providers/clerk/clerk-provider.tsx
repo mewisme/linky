ed404600fa9f7829c35b05b29a59b89cb6e2b05b
Mew <mauminh.nguyen@gmail.com>
2026-01-25 19:48:10 +0700

    refactor: simplify theme handling in ClerkProvider component
    - Removed the useTheme hook and directly set the theme to dark in the ClerkProvider component, streamlining the theme configuration for improved clarity and maintainability.

--- apps/web/src/components/providers/clerk/clerk-provider.tsx
+++ apps/web/src/components/providers/clerk/clerk-provider.tsx
@@ -1,14 +1,10 @@
-"use client";
-
 import { ClerkProvider as NextClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
-import { useTheme } from "next-themes";
 
 export function ClerkProvider({ children }: { children: React.ReactNode }) {
-  const { theme } = useTheme();
   return (
     <NextClerkProvider appearance={{
-      theme: theme === "dark" ? dark : undefined,
+      theme: dark,
     }}>
       {children}
     </NextClerkProvider>

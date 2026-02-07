25b0cea7e0f16c45bcd2c812d40cf7ff9501b0d8
Mew <mauminh.nguyen@gmail.com>
2026-02-07 15:33:29 +0700

    chore: remove sourcemap and dts options from tsup configuration
    - Removed `sourcemap` and `dts` options from the `tsup.config.ts` file to streamline the build process and reduce output size.

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -4,8 +4,6 @@ export default defineConfig({
   entry: ["src/index.ts"],
   outDir: "dist",
   format: "esm",
-  sourcemap: true,
-  dts: true,
   minifyIdentifiers: true,
   minifySyntax: true,
   clean: true,

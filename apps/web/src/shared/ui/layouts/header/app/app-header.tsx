ae5e0eb4487bfd704f16643fba85d57c7b1af02a
Mew <mauminh.nguyen@gmail.com>
2026-03-01 22:43:02 +0700

    refactor(header): update layout and spacing in AppHeader component
    - Adjusted the layout of the AppHeader to use a grid system for better alignment.
    - Removed the ModeToggle for a cleaner interface.
    - Enhanced spacing and alignment for the CommandMenu and NotificationsBell components.

--- apps/web/src/shared/ui/layouts/header/app/app-header.tsx
+++ apps/web/src/shared/ui/layouts/header/app/app-header.tsx
@@ -4,7 +4,6 @@ import { Button } from "@ws/ui/components/ui/button"
 import { CommandMenu } from '@/shared/ui/layouts/header/command-menu'
 import Link from "next/link"
 import { Logo } from "../landing/logo"
-import { ModeToggle } from "../mode-toggle"
 import { NotificationsBell } from "../notifications-bell"
 import { SidebarTrigger } from '@ws/ui/components/animate-ui/components/radix/sidebar'
 import { cn } from "@ws/ui/lib/utils"
@@ -12,34 +11,38 @@ import { useIsMobile } from "@ws/ui/hooks/use-mobile"
 
 export function AppHeader() {
   const isMobile = useIsMobile()
+
   return (
     <header
       className={cn(
-        "sticky top-0 z-20 w-full h-16 bg-background container mx-auto transition-[border-color]",
+        "sticky top-0 z-20 h-16 w-full bg-background container mx-auto"
       )}
     >
-      <div className='h-full flex items-center justify-between space-x-4 mx-4'>
-        <div className='flex items-center space-x-4'>
-          <Button asChild variant='outline' size='icon'>
+      <div className="grid grid-cols-3 items-center h-full mx-4">
+
+        <div className="flex items-center space-x-4">
+          <Button asChild variant="outline" size="icon">
             <SidebarTrigger />
           </Button>
+
           {!isMobile && (
             <Link href="/">
-              <Logo
-                size="sm"
-                className="cursor-pointer"
-              />
+              <Logo size="sm" className="cursor-pointer" />
             </Link>
           )}
         </div>
-        <div className='max-w-md w-full'>
-          <CommandMenu />
+
+        <div className="flex justify-center">
+          <div className="w-full max-w-md">
+            <CommandMenu />
+          </div>
         </div>
-        <div className='flex items-center space-x-4'>
-          <div className="w-16" />
+
+        <div className="flex items-center justify-end space-x-4">
           <NotificationsBell />
         </div>
+
       </div>
-    </header >
+    </header>
   )
 }
